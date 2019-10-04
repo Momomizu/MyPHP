ff22565606dfb934c3acc727165809bb332ba9b5
Ji Min <jiminbestfriendboyfriend@gmail.com>
2019-10-04 15:43:14 +0700

    I have new code

--- index.php
+++ index.php
@@ -5,8 +5,38 @@
 <h1>My first PHP page</h1>
 
 <?php
-echo "Hello World!";
+echo "Show all rows from Postgres Database";
+
+//Refer to database
+$db = parse_url(getenv("DATABASE_URL"));
+
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
+//your sql query
+$sql = "SELECT studentname, course FROM RegisterCourse";
+
+$stmt = $pdo -> prepare($sql);
+
+//execute the query on the server and return the result set
+$stmt -> setFetchMode (PFO::FETCH_ASSOC);
+$stmt -> execute();
+$resultSet = $stmt -> fetchAll();
+
+//display the data
 ?>
+<ul>
+	<?php
+		foreach ($resultSet as $row) {
+			echo "<li>" . $row["studentname"] . '--' . $row["course"] . "</li>";
+		}
+	?>
+</ul>
 
 </body>
 </html>
